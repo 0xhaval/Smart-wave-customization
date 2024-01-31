b2702575974131b3042a6f0d519440520843d6d4
0xhaval <oomer5961@gmail.com>
2024-02-01 00:42:32 +0300

    feat: add print format dl

--- smart_wave_customize/hooks.py
+++ smart_wave_customize/hooks.py
@@ -221,7 +221,7 @@ fixtures = [
     {
       "dt": "Print Format",
         "filters": [
-            ["name", "in", ["Smart Wave SI", "Smart Wave PO"]]
+            ["name", "in", ["Smart Wave SI", "Smart Wave PO", "Smart wave LD"]]
         ]
     },
 
